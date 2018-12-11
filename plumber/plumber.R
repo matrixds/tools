f46f4b9c7461064b038b7719bb4e9f113b403a76
Isaac Faber <isaacfab@gmail.com>
2018-12-10 16:26:56 -0800

    updating some plumber stuff

--- plumber/plumber.R
+++ plumber/plumber.R
@@ -5,12 +5,19 @@
 
 # plumber.R
 
+#* Echo back the input
+#* @param msg The message to echo
+#* @get /echo
+function(msg=""){
+  list(msg = paste0("The message is: '", msg, "'"))
+}
 
 #* Plot a histogram
 #* @png
-#* @get /test
-function()
-  return('It is alive!')
+#* @get /plot
+function(){
+  rand <- rnorm(100)
+  hist(rand)
 }
 
 #* Return the sum of two numbers
@@ -20,3 +27,4 @@ function()
 function(a, b){
   as.numeric(a) + as.numeric(b)
 }
+
