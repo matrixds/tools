58bad790e8f9d4a0506654a1b31262399160b483
JvReddy1 <47448758+JvReddy1@users.noreply.github.com>
2019-03-20 21:19:34 +0530

    Updating entrypoint.sh

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -2,6 +2,21 @@
 set -e
 
 mkdir -p /var/log/nginx
-mkdir -p /usr/share/nginx/html
+#mkdir -p /usr/share/nginx/html
+
+
+if [ ! -d "/usr/share/nginx/html" ]
+then
+  mkdir -p /usr/share/nginx/html
+  cp -r /index.html /usr/share/nginx/html/
+
+else
+  if [ ! "$(ls -A /usr/share/nginx/html)" ]
+  then
+     cp -r /index.html /usr/share/nginx/html/
+  fi
+fi
+
+
 
 nginx -g "daemon off;"
