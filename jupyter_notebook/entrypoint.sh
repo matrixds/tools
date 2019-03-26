045934e039129b35eac6c7e3b9993f88adef0f65
Isaac Faber <isaacfab@gmail.com>
2019-03-25 23:36:38 -0700

    adding two new tools

---
+++ jupyter_notebook/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+cd $HOME
+
+mkdir -p $HOME/.jupyter
+cp /app/jupyter_notebook_config.py $HOME/.jupyter/.
+
+mkdir -p /app/jupyter/runtime
+mkdir -p /app/jupyter/ipython
+mkdir -p $HOME/.pip
+
+[[ -f $HOME/.pip/pip.conf ]] || cp /app/pip.conf $HOME/.pip/.
+
+export JUPYTER_RUNTIME_DIR=/app/jupyter/runtime
+export IPYTHONDIR=/app/jupyter/ipython
+
+jupyter notebook # --debug
---
+++ tensorboard/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p /home/matrix/tensorboard_logs
+
+tensorboard --logdir=/home/matrix/tensorboard_logs
