efe1c0abf48ca18f8287955745a1eb088680fc83
Isaac Faber <isaacfab@gmail.com>
2020-02-22 16:41:36 -0500

    some updates

--- rbase/packages.R
+++ rbase/packages.R
@@ -1,12 +1,11 @@
 #Script for common package installation on MatrixDS docker image
 p<-c('nnet','kknn','randomForest','xgboost','tidyverse','plotly','shiny','shinydashboard',
-	  'devtools','FinCal','googleVis','DT', 'kernlab','earth',
+	  'devtools','DT', 'kernlab', 'parsnip', 'tidymodels',
      'htmlwidgets','rmarkdown','lubridate','leaflet','sparklyr','magrittr','openxlsx',
      'packrat','roxygen2','knitr','readr','readxl','stringr','broom','feather',
-     'forcats','testthat','plumber','RCurl','rvest','mailR','nlme','foreign','lattice',
-     'expm','Matrix','flexdashboard','caret','mlbench','plotROC','RJDBC','rgdal',
-     'highcharter','tidyquant','timetk','quantmod','PerformanceAnalytics','scales',
-     'tidymodels','C50','xgboost','parsnip','rmetalog','reticulate')
+     'forcats','testthat','plumber','RCurl','rvest','nlme','foreign','lattice',
+     'expm','Matrix','flexdashboard','mlbench','plotROC','RJDBC','rgdal',
+     'highcharter','timetk','scales', 'C50','xgboost','reticulate')
 
 
 install.packages(p,dependencies = TRUE)
